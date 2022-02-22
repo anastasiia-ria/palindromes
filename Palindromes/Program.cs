1c72ba4e51b4221010c78815b14ced5d85fd9f5f
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-22 14:07:34 -0800

    add UI for palindrome program
    Co-authored-by: Alex Adamovic <alexanderadamovic@gmail.com>

--- Palindromes/Program.cs
+++ Palindromes/Program.cs
@@ -7,9 +7,28 @@ namespace Palindromes
   {
     public static void Main()
     {
-      string word = "read";
-      Palindrome testPalindrome = new Palindrome(word);
-      testPalindrome.IsPalindrome();
+      Console.WriteLine("Please enter a word, phrase, or number:");
+      string userWord = Console.ReadLine();
+
+      Palindrome userPalindrome = new Palindrome(userWord);
+      if (userPalindrome.IsPalindrome() == true)
+      {
+        Console.WriteLine("Congratulations!! You have a palindrome");
+      }
+      else
+      {
+        Console.WriteLine("Try again. This is not a palindrome");
+      }
+      Console.WriteLine("Press 1 to try again or enter anything else to exit");
+      string again = Console.ReadLine();
+      if (again == "1")
+      {
+        Main();
+      }
+      else
+      {
+        Console.WriteLine("Okay, bye");
+      }
     }
   }
 }
